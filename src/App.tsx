2cb4a8c9d95853c27e5810faa9abd716fbf94910
Slav Lomachynkyi <slavchek.3777@gmail.com>
2024-01-09 05:20:26 +0200

    end Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbat";
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import items from "../data/items.json";
+
+const Store = () => {
+  return (
+    <>
+      <h1>Store</h1>
+    </>
+  );
+};
+
+export default Store;
