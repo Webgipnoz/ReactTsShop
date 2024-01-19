bb11456cca16890c2493a54199e04d35a6ab6b00
Slav Lomachynkyi <slavchek.3777@gmail.com>
2024-01-19 23:21:31 +0200

    work with order page

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { selectItem } from "../redux/slices/itemsSlice";
+import StoreItem from "../components/StoreItem";
+import orderItem from "../data/items.json";
 
 const OrderPage = () => {
-  return <h1>Order</h1>;
+  const { cartItems } = useSelector(selectItem);
+
+  return (
+    <>
+      <h1>Order123</h1>
+      <>123</>
+      {/* <Row md={2} xs={1} lg={3} className="g-3">
+        {cartItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem
+              id={item.id}
+              name={orderItem[item.id].name}
+              price={orderItem[item.id].price}
+              imgUrl={orderItem[item.id].imgUrl}
+            />
+          </Col>
+        ))}
+      </Row> */}
+    </>
+  );
 };
 
 export default OrderPage;
